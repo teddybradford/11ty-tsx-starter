7c7b7fc56cf61b546e4d57c695a1df938ec438a7
Teddy Bradford <3684553+teddybradford@users.noreply.github.com>
2025-09-15 15:21:57 -0400

    Update html meta tags

--- src/_includes/layout.11ty.tsx
+++ src/_includes/layout.11ty.tsx
@@ -8,7 +8,7 @@ export default function Layout({ title, content }: LayoutProps) {
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="viewport" content="width=device-width" />
         <meta name="color-scheme" content="light dark" />
         <title>{title}</title>
         <link rel="stylesheet" href="/styles/global.css" />
